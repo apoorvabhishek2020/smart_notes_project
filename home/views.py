106d264b331c97b1ebeccbeb7b86317ee4d48ba3
apoorvabhishek2020 <apoorvabhishek2020@gmail.com>
2022-12-17 15:54:37 +0530

    hello smart notes

--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from datetime import datetime
 def home(request):
-    return HttpResponse('<h1>Hello, World!')
-
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
